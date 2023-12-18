854a267b7973ca65ab378ae3c7c28b5bf300beb6
Dhanarooban1 <dhanazx1@gmail.com>
2023-12-18 16:58:30 +0530

    completed

--- README.md
+++ README.md
@@ -4,3 +4,17 @@
 
 1) Home page
 ![Screenshot 2023-12-18 083857](https://github.com/Dhanarooban1/Game-Development-Quiz/assets/146908898/8b794060-392d-4921-9810-8fc453db2440)
+
+2) Instructions Page
+
+![Screenshot 2023-12-18 165501](https://github.com/Dhanarooban1/Game-Development-Quiz/assets/146908898/9e6c63ce-ead6-449f-b2b5-e51eb897041e)
+
+
+
+3) Game page
+
+![Screenshot 2023-12-18 083927](https://github.com/Dhanarooban1/Game-Development-Quiz/assets/146908898/eb9fb080-fe37-4af6-a4c3-636ddab0215b)
+
+
+4) Game Over page
+![Screenshot 2023-12-18 083936](https://github.com/Dhanarooban1/Game-Development-Quiz/assets/146908898/44d18582-6de2-466f-a5d8-6ccce6d19e50)
--- game.js
+++ game.js
@@ -27,51 +27,57 @@ let score = 0;
 let time = 60;
 
 let data = [
-  // {
-  //   name: "What is h20?",
-  //   ans: 0,
-  //   images: [
-  //     "assets/images/que1/water.png",
-  //     "assets/images/que1/iron.png",
-  //     "assets/images/que1/copper.png",
-  //   ],
-  // },
-  // {
-  //   name: "What element is a key component in the production of glass used in windows and mirrors?",
-  //   ans: 1,
-  //   images: [
-  //     "assets/images/que2/calcium.png",
-  //     "assets/images/que2/silicon.png",
-  //     "assets/images/que2/sodium.png",
-  //   ],
-  // },
-  // {
-  //   name: "In rechargeable batteries found in many electronic devices, which element is commonly used in the cathode?",
-  //   ans: 1,
-  //   images: [
-  //     "assets/images/que3/zinc.png",
-  //     "assets/images/que3/lithium.png",
-  //     "assets/images/que3/nickel.png",
-  //   ],
-  // },
-  // {
-  //   name: "What metal is often alloyed with copper to create brass, a widely used material in the manufacturing of musical instruments and decorative items?",
-  //   ans: 2,
-  //   images: [
-  //     "assets/images/que4/nickel.png",
-  //     "assets/images/que4/aluminium.png",
-  //     "assets/images/que4/zinc.png",
-  //   ],
-  // },
-  // {
-  //   name: "In the semiconductor industry, what element is a fundamental component of silicon wafers used in electronic devices?",
-  //   ans: 1,
-  //   images: [
-  //     "assets/images/que4/nickel.png",
-  //     "assets/images/que2/silicon.png",
-  //     "assets/images/que3/zinc.png",
-  //   ],
-  // },
+  // 1
+  {
+    name: "What is h20?",
+    ans: 0,
+    images: [
+      "assets/images/que1/water.png",
+      "assets/images/que1/iron.png",
+      "assets/images/que1/copper.png",
+    ],
+  },
+  // 2
+  {
+    name: "What element is a key component in the production of glass used in windows and mirrors?",
+    ans: 1,
+    images: [
+      "assets/images/que2/calcium.png",
+      "assets/images/que2/silicon.png",
+      "assets/images/que2/sodium.png",
+    ],
+  },
+  // 3
+  {
+    name: "In rechargeable batteries found in many electronic devices, which element is commonly used in the cathode?",
+    ans: 1,
+    images: [
+      "assets/images/que3/zinc.png",
+      "assets/images/que3/lithium.png",
+      "assets/images/que3/nickel.png",
+    ],
+  },
+  // 4
+  {
+    name: "What metal is often alloyed with copper to create brass, a widely used material in the manufacturing of musical instruments and decorative items?",
+    ans: 2,
+    images: [
+      "assets/images/que4/nickel.png",
+      "assets/images/que4/aluminium.png",
+      "assets/images/que4/zinc.png",
+    ],
+  },
+  // 5
+  {
+    name: "In the semiconductor industry, what element is a fundamental component of silicon wafers used in electronic devices?",
+    ans: 1,
+    images: [
+      "assets/images/que4/nickel.png",
+      "assets/images/que2/silicon.png",
+      "assets/images/que3/zinc.png",
+    ],
+  },
+  // 6
   {
     name: "Which element is commonly used as a fuel in car engines?",
     ans: 1,
@@ -81,6 +87,7 @@ let data = [
       "assets/images/que6/titanium.png",
     ],
   },
+  // 7
   {
     name: "Which element is commonly used in the construction of car bodies to prevent corrosion?",
     ans: 0,
@@ -90,13 +97,34 @@ let data = [
       "assets/images/que6/titanium.png",
     ],
   },
+  // 8
   {
     name: "What is the chemical symbol for the element used in car tires to enhance their elasticity and is also a scripting language commonly used in web development?",
     ans: 0,
     images: [
-      "assets/images/que4/aluminium.png",
+      "assets/images/que8/sulfur.png",
       "assets/images/que1/copper.png",
       "assets/images/que6/titanium.png",
+    ],
+  },
+  // 9
+  {
+    name: "Which gas is responsible for inflating airbags in cars during a collision and is also a commonly used command-line interface in computer operating systems?",
+    ans: 1,
+    images: [
+      "assets/images/que9/ethyl.png",
+      "assets/images/que9/nitrogen.png",
+      "assets/images/que6/oxygen.png",
+    ],
+  },
+  // 10
+  {
+    name: "What is the pH value of pure water?",
+    ans: 1,
+    images: [
+      "assets/images/que10/number-10.png",
+      "assets/images/que10/number-5.png",
+      "assets/images/que10/7.png",
     ],
   },
 ];
